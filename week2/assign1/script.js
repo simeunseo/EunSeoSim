b498e69594caeddc6397c0c9b6f67baaeab595a0
simeunseo <rkdcjfehowl@naver.com>
2023-04-16 22:41:41 +0900

    feat : 상수값으로 저장된 데이터로 card 동적 생성

--- week2/assign1/script.js
+++ week2/assign1/script.js
@@ -3,16 +3,20 @@ import ITEM_LIST from "./data/item_data.js";
 const cardsSection = document.getElementById("cards");
 const cardTemplate = document.getElementById("cards__template");
 
-let content = cardTemplate.cloneNode(true);
-let newHtml = content.innerHTML;
-newHtml = newHtml
-  .replace("{item_name}", "청경채")
-  .replace("{tags}", `<small>푸릇푸릇</small>\n<small>신선</small>`)
-  .replace("{img_alt}", "청경채")
-  .replace(
-    "{img_src}",
-    "https://img-cf.kurly.com/shop/data/goods/1619680685239l0.jpg"
-  );
+ITEM_LIST.map((item, idx) => {
+  let tags = ``;
+  item.tags.map((tag, idx) => {
+    tags += `<small>` + tag + `</small>\n`;
+  });
 
-content.innerHTML = newHtml;
-cardsSection.appendChild(content.content);
+  let content = cardTemplate.cloneNode(true);
+  let newHtml = content.innerHTML;
+  newHtml = newHtml
+    .replace("{item_name}", item.name)
+    .replace("{tags}", tags)
+    .replace("{img_alt}", item.name)
+    .replace("{img_src}", item.img);
+
+  content.innerHTML = newHtml;
+  cardsSection.appendChild(content.content);
+});
