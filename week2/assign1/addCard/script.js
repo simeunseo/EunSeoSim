a31af94cddaf913eb82041115d6af14017d9a0da
simeunseo <rkdcjfehowl@naver.com>
2023-04-18 03:09:32 +0900

    style : 이미지 미리보기 및 form css 완료

--- week2/assign1/addCard/script.js
+++ week2/assign1/addCard/script.js
@@ -1,7 +1,18 @@
+const imageInput = document.getElementById("image");
+const imageThumbNail = document.getElementById("image-thumbnail");
+imageInput.addEventListener("input", () => {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    imageThumbNail.src = reader.result;
+  });
+  reader.readAsDataURL(imageInput.files[0]);
+});
+
 const form = document.getElementById("add-card-form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const name = e.target.name.value;
   const hashTags = e.target.hashtag.value.replace(/ /g, "").split(",");
   const category = e.target.category.value;
+  console.log(e.target.image.value);
 });
