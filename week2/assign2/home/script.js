99d2072f355453aef2824349a5ebf449f3eec716
simeunseo <rkdcjfehowl@naver.com>
2023-04-19 01:33:12 +0900

    feat : 할일 체크/체크해제와 미완료 할일 수 표시

--- week2/assign2/home/script.js
+++ week2/assign2/home/script.js
@@ -3,13 +3,38 @@ import TODO_DATA from "../common/todoData.js";
 const COLOR_LIST = ["red", "green", "yellow", "blue"];
 
 let todoData = []; //localStorage에 저장된 목록을 가져와 저장하는 배열
+let todoCounting = 0; //미완료 할일의 수
 
 window.onload = () => {
   localStorage.getItem("todo_data") === null &&
     localStorage.setItem("todo_data", JSON.stringify(TODO_DATA)); //localStorage 초기화
   todoData = JSON.parse(localStorage.getItem("todo_data")); //localStorage에 저장된 목록을 가져옴
   listToTodo(todoData);
+  todoCount();
+  checkDone();
 };
+
+//할일 클릭을 감지하여 처리하는 함수
+function checkDone() {
+  const doneBtn = document.getElementsByClassName("done-btn");
+  const doneBtnList = [...doneBtn]; //HTMLCollection to Array
+
+  doneBtnList.forEach((item) => {
+    item.addEventListener("click", () => {
+      let doneValue = item.attributes.done.value;
+      doneValue === "true"
+        ? ((item.attributes.done.value = "false"), todoCounting++)
+        : ((item.attributes.done.value = "true"), todoCounting--);
+      todoCount();
+    });
+  });
+}
+
+//미완료 할일의 수를 표시하는 함수
+function todoCount() {
+  const todayTodoCounting = document.getElementById("today__todoCounting");
+  todayTodoCounting.innerText = todoCounting;
+}
 
 //list를 탐색하면서 요소를 하나씩 투두로 만드는 함수
 function listToTodo(list) {
@@ -36,6 +61,7 @@ function listToTodoList(list) {
   const todoListTemplate = document.getElementById("todo__list__template"); //todo list(한 카테고리의 투두 목록들) 템플릿
   let finalHtml = "";
   list.forEach((item) => {
+    item.done === false && todoCounting++;
     let todoListContent = todoListTemplate.cloneNode(true);
     let todoListNewHtml = todoListContent.innerHTML;
     todoListNewHtml = todoListNewHtml
