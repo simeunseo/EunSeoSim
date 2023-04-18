3f172982a648a8619dab9a660877e5d7f0c14a49
simeunseo <rkdcjfehowl@naver.com>
2023-04-19 02:32:37 +0900

    feat : 할일 체크 여부를 localStorage에도 반영

--- week2/assign2/home/script.js
+++ week2/assign2/home/script.js
@@ -16,15 +16,32 @@ window.onload = () => {
 
 //할일 클릭을 감지하여 처리하는 함수
 function checkDone() {
+  let localStorageData = JSON.parse(localStorage.getItem("todo_data")); //변경된 사항을 localStorage에 반영함
+
   const doneBtn = document.getElementsByClassName("done-btn");
   const doneBtnList = [...doneBtn]; //HTMLCollection to Array
 
   doneBtnList.forEach((item) => {
     item.addEventListener("click", () => {
-      let doneValue = item.attributes.done.value;
-      doneValue === "true"
-        ? ((item.attributes.done.value = "false"), todoCounting++)
-        : ((item.attributes.done.value = "true"), todoCounting--);
+      const changedTodo = item.children[0].id; //클릭된 todo 항목의 이름
+      const changedCategory = item.nextElementSibling.attributes.category.value; //클릭된 todo 항목의 카테고리명
+      const doneValue = item.attributes.done.value;
+      if (doneValue === "true") {
+        //localStorageData에서 카테고리명이 changedCategory이고 항목 이름이 changedTodo인 요소의 done값을 변경한다!
+        //사실 item 전부 반복문 돌려서 찾으면 가독성은 더 좋은데, 반복문을 피하려고 하다보니 이렇게 긴 탐색의 과정이 필요했습니다...
+        localStorageData
+          .find((item) => item.category === changedCategory)
+          .list.find((item) => item.content === changedTodo).done = "false";
+        item.attributes.done.value = "false";
+        todoCounting++;
+      } else {
+        localStorageData
+          .find((item) => item.category === changedCategory)
+          .list.find((item) => item.content === changedTodo).done = "true";
+        item.attributes.done.value = "true";
+        todoCounting--;
+      }
+      localStorage.setItem("todo_data", JSON.stringify(localStorageData));
       todoCount();
     });
   });
@@ -42,7 +59,7 @@ function listToTodo(list) {
   const todoTemplate = document.getElementById("todo__template"); //todo(전체 박스) 템플릿
   todoSection.replaceChildren();
   list.forEach((item, idx) => {
-    let todoListNewHtml = listToTodoList(item.list);
+    let todoListNewHtml = listToTodoList(item.list, item.category);
 
     let todoContent = todoTemplate.cloneNode(true); //템플릿 복사
     let todoNewHtml = todoContent.innerHTML; //템플릿 안의 html 복사
@@ -57,16 +74,17 @@ function listToTodo(list) {
   });
 }
 
-function listToTodoList(list) {
+function listToTodoList(list, categoryName) {
   const todoListTemplate = document.getElementById("todo__list__template"); //todo list(한 카테고리의 투두 목록들) 템플릿
   let finalHtml = "";
   list.forEach((item) => {
-    item.done === false && todoCounting++;
+    item.done === "false" && todoCounting++;
     let todoListContent = todoListTemplate.cloneNode(true);
     let todoListNewHtml = todoListContent.innerHTML;
     todoListNewHtml = todoListNewHtml
       .replace("{done}", item.done)
-      .replace("{todo_content}", item.content);
+      .replace("{category_name}", categoryName)
+      .replace(/{todo_content}/gi, item.content);
     finalHtml += todoListNewHtml;
   });
 
