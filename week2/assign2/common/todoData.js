90a7eaec039c9b7e25b9e25c7bad776150f54039
simeunseo <rkdcjfehowl@naver.com>
2023-04-19 00:55:56 +0900

    feat : 상수값으로 저장된 데이터로 투두리스트 동적 생성

---
+++ week2/assign2/common/todoData.js
@@ -0,0 +1,52 @@
+const TODO_DATA = [
+  {
+    category: "과제",
+    list: [
+      {
+        content: "db 프로젝트",
+        done: true,
+      },
+      {
+        content: "pl 과제",
+        done: true,
+      },
+    ],
+  },
+  {
+    category: "공부",
+    list: [
+      {
+        content: "이산구조 복습",
+        done: false,
+      },
+      {
+        content: "react 강의",
+        done: true,
+      },
+    ],
+  },
+  {
+    category: "일과",
+    list: [
+      {
+        content: "일기 쓰기",
+        done: false,
+      },
+      {
+        content: "운동 1시간",
+        done: false,
+      },
+    ],
+  },
+  {
+    category: "동아리",
+    list: [
+      {
+        content: "솝트 스터디 신청",
+        done: true,
+      },
+    ],
+  },
+];
+
+export default TODO_DATA;
