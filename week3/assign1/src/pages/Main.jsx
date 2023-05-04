ca70560d41cdd6e195cfce63b735bed51beb83a2
simeunseo <rkdcjfehowl@naver.com>
2023-05-05 05:10:53 +0900

    feat : 오디오 플레이어

--- week3/assign1/src/pages/Main.jsx
+++ week3/assign1/src/pages/Main.jsx
@@ -1,9 +1,9 @@
+import Audio from "../components/Audio";
 import BGM from "../assets/bgm.mp3";
 import Cards from "../components/Cards";
 import { GlobalContextProvider } from "../context/context";
 import Header from "../components/Header";
 import { useState } from "react";
-
 const Main = () => {
   const [compareList, setCompareList] = useState([]); // 비교 대상인 카드의 {pk, imgId}를 저장
   const [pairedList, setPairedList] = useState([]); // 짝을 맞춘 카드의 {pk, imgId}를 저장
@@ -11,14 +11,7 @@ const Main = () => {
 
   return (
     <GlobalContextProvider>
-      <audio
-        autoPlay
-        onLoadStart={(e) => {
-          e.target.volume = 0.01;
-        }}
-      >
-        <source src={BGM} type="audio/mp3" />
-      </audio>
+      <Audio bgm={BGM} />
       <Header
         compareList={compareList}
         setCompareList={setCompareList}
