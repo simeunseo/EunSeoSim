72472757b2b9b809bdc5ec2fcf1d1ef289835846
simeunseo <rkdcjfehowl@naver.com>
2023-05-10 03:36:27 +0900

    feat : 날씨 검색 form 추가

--- week4/assign1/src/components/Search.jsx
+++ week4/assign1/src/components/Search.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <form>
+      <select>
+        <option value="오늘">오늘</option>
+        <option value="주간">주간</option>
+      </select>
+      <input placeholder="영문 지역명 ex)bucheon" />
+      <button type="submit">검색</button>
+    </form>
+  );
 };
 
 export default Search;
--- week4/assign1/src/pages/Main.jsx
+++ week4/assign1/src/pages/Main.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 import PageLayout from "../components/PageLayout";
+import Search from "../components/Search";
 import WeaderCard from "../components/WeaderCard";
-
 const Main = () => {
   return (
     <PageLayout>
+      <Search />
       <WeaderCard />
       <Outlet />
     </PageLayout>
