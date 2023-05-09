6f06bb5bb73d422ddcf8bf272041b00ca68fe766
simeunseo <rkdcjfehowl@naver.com>
2023-05-10 06:57:57 +0900

    feat : 주간 날씨에서 오늘 날씨의 경우 시간대별 예보 중 가장 첫번째 값을, 오늘 날짜가 아닌 경우 12:00:00시의 값을 보여주도록 처리

--- week4/assign1/src/components/WeaderCard.jsx
+++ week4/assign1/src/components/WeaderCard.jsx
@@ -15,8 +15,39 @@ const WeaderCard = () => {
       clouds: 0.0,
     },
   ]);
+
   const apiType = type === "today" ? "weather" : "forecast";
   const getWeaderInfo = () => {
+    let tmpWeatherData = [];
+    const today = new Date();
+    const todayYear = today.getFullYear();
+    const todayMonth = String(today.getMonth() + 1).padStart(2, "0");
+    const todayDate = String(today.getDate()).padStart(2, "0");
+    const comparingDate = `${todayYear}-${todayMonth}-${todayDate}`;
+    const comparingTime = "12:00:00";
+    /*
+      주간 날씨 데이터에서,
+      오늘 날짜의 경우 각 시간대별 예보 중 가장 첫번째 값을,
+      오늘 날짜가 아닌 경우 12:00:00시의 예보 값을 가져와
+      5일치의 데이터를 정리하는 함수
+    */
+    const filterWeatherData = (weatherDataList) => {
+      let filteredDataList = {};
+
+      filteredDataList = [
+        weatherDataList[0],
+        ...weatherDataList
+          .filter((data) => {
+            return !data.dt_txt.includes(comparingDate);
+          })
+          .filter((data) => {
+            return data.dt_txt.includes(comparingTime);
+          }),
+      ];
+
+      return filteredDataList;
+    };
+
     fetch(
       `https://api.openweathermap.org/data/2.5/${apiType}?q=${area}&appid=${
         import.meta.env.VITE_APP_WEATHER
@@ -25,11 +56,8 @@ const WeaderCard = () => {
       .then((res) => res.json())
       .then((data) => {
         if (data.cod == 200) {
+          console.log(data);
           // 주간 데이터에는 '200'으로, 일간 데이터에는 200으로 되어있기에 ===이 아닌 ==을 사용
-          let tmpWeatherData = [];
-          const today = new Date();
-          const todayMonth = today.getMonth() + 1;
-          const todayDate = today.getDate();
 
           switch (type) {
             case "today":
@@ -47,22 +75,18 @@ const WeaderCard = () => {
               ]);
               break;
             case "week":
-              data.list
-                .filter((i, idx) => {
-                  return idx % 8 === 1 ? i : undefined; //5일간의 정보를 받아오기 위한 parsing
-                })
-                .map((data, idx) => {
-                  tmpWeatherData.push({
-                    id: idx,
-                    date: `${todayMonth}/${todayDate + idx}`,
-                    weather: data.weather.main,
-                    temp: data.main.temp,
-                    feels_like: data.main.feels_like,
-                    temp_min: data.main.temp_min,
-                    temp_max: data.main.temp_max,
-                    clouds: data.clouds.all,
-                  });
+              filterWeatherData(data.list).map((data, idx) => {
+                tmpWeatherData.push({
+                  id: idx,
+                  date: `${todayMonth}/${parseInt(todayDate) + idx}`,
+                  weather: data.weather.main,
+                  temp: data.main.temp,
+                  feels_like: data.main.feels_like,
+                  temp_min: data.main.temp_min,
+                  temp_max: data.main.temp_max,
+                  clouds: data.clouds.all,
                 });
+              });
               setWeaderData(tmpWeatherData);
               break;
           }
