873867e555c4cdf9fc3fb55af60659e5b74d0aff
simeunseo <rkdcjfehowl@naver.com>
2023-06-10 23:26:24 +0900

    리팩토링 전 파일 삭제

--- week7/assign1/src/components/ModalPortal.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDom from "react-dom";
-import { ReactNode } from "react";
-
-const ModalPortal = ({ children }) => {
-  const el = document.getElementById("modal-root");
-  return ReactDom.createPortal(children, el);
-};
-
-export default ModalPortal;
